06b4c3a50adf78964eb04cec58e9797cc5cca70a
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-04 20:53:19 +0300

    remove IUnit interaface and implement PhysicsUnit component for simulate physics

--- Assets/Scripts/Components/PhysicsUnit.cs
+++ Assets/Scripts/Components/PhysicsUnit.cs
@@ -1,21 +1,31 @@
-using Interfaces;
 using UnityEngine;
 
 namespace Components
 {
-    public class PhysicsUnit : BaseUnit
+    public class PhysicsUnit : MonoBehaviour
     {
+        [SerializeField] private bool simulate;
+        [Space]
+        [SerializeField] private float mass;
         [SerializeField] private Vector3 velocity;
+        
+        private const float Gravity = 9.8f;
 
-        public Vector3 Velocity
+        private void FixedUpdate()
         {
-            get => velocity;
-            set => velocity = value;
+            if (!simulate) return;
+            Simulate(); 
         }
 
-        public override void Move()
+        private void Simulate()
         {
-            
+             velocity -= new Vector3(0, Gravity * mass, 0);
+             transform.position += velocity * Time.fixedDeltaTime;           
+        }
+
+        public void AddForce2D(float angleInRad, float force)
+        {
+            velocity += new Vector3(Mathf.Cos(angleInRad), Mathf.Sin(angleInRad)) * force;
         }
     }
 }
