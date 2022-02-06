46893c5d13cd0f94acd2d96754fb2a3e8a0fdd4c
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-07 00:33:50 +0300

    fix: remove connecting collider to collision controller from CircleCollider

--- Assets/Scripts/Components/CircleCollider.cs
+++ Assets/Scripts/Components/CircleCollider.cs
@@ -1,4 +1,3 @@
-using System;
 using Interfaces;
 using UnityEngine;
 
@@ -10,25 +9,13 @@ namespace Components
         [SerializeField] private float radius;
 
         private Vector3 _position;
-        private CollisionController _collisionController;
 
         public Vector2 Center => _position;
         public float Radius => radius;
         
-        private void Start()
-        {
-            _collisionController = FindObjectOfType<CollisionController>();
-            _collisionController.AddCollider(this);
-        }
-
         private void FixedUpdate()
         {
             _position = transform.position + (Vector3) offset;
-        }
-
-        private void OnDestroy()
-        {
-           _collisionController.RemoveCollider(this); 
         }
 
 #if UNITY_EDITOR 
