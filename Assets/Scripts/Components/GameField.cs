ee51f76137f57e15dc4bbc005e95e2a115d7ce93
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-06 13:18:03 +0300

    added: logic by CollisionExit event on SquareCollider

--- Assets/Scripts/Components/GameField.cs
+++ Assets/Scripts/Components/GameField.cs
@@ -6,8 +6,19 @@ namespace Components
     public class GameField : MonoBehaviour
     {
         [SerializeField] private Camera screenSpace;
+        [SerializeField] private SquareCollider squareCollider;
 
         // TODO: configuration game field by viewport space and screen space
+        
+        private void Start()
+        {
+            squareCollider.CollisionExit += CollisionExit;
+        }
+        
+        private static void CollisionExit(Collision info)
+        {
+            Destroy(info.Collider.gameObject);
+        }
         
         public Vector3 ViewportToGameField(Vector2 viewportPoint)
         {
