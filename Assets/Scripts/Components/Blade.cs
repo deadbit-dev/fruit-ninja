c1622d5af95b9aa1fc70043cf2db7b91b82c43df
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-14 03:31:48 +0300

    update logic slice system

--- Assets/Scripts/Components/Blade.cs
+++ Assets/Scripts/Components/Blade.cs
@@ -27,36 +27,38 @@ namespace Components
                 return;
             }
            
-            Slice(info.Collider.gameObject);
+            Slice(info.Collider.gameObject, info.ContactPosition);
         }
 
-        private void Slice(GameObject unit)
+        private void Slice(GameObject unit, Vector3 contactSlice)
         {
-             var unitTransform = unit.transform;
-             var unitPosition = unitTransform.position;
-             var unitRotation = unitTransform.rotation;
-             var unitLocalScale = unitTransform.localScale;
-             var sliceSprite = unit.GetComponent<SpriteRenderer>().sprite;
-             
-             // TODO: direction
-             var sliceDirection = new Vector2();
+            var unitTransform = unit.transform;
+            var unitPosition = unitTransform.position;
+            var unitRotation = unitTransform.rotation;
+            var unitLocalScale = unitTransform.localScale;
+            var sliceSprite = unit.GetComponent<SpriteRenderer>().sprite;
+
+            var contactSliceUV = RuntimeSpriteEditor.WorldPointToSpriteUV(sliceSprite, contactSlice, unitTransform);
+            var spritePivotUV = RuntimeSpriteEditor.SpritePivotUV(sliceSprite);
+
+            // TODO: sliceSecant by contactSliceUV and spritePivotUV
+            var sliceSecant = new Vector2();
+
+            var (spriteA, spriteB) = RuntimeSpriteEditor.SpriteSlice(sliceSprite, sliceSecant);
  
-             var (spriteA, spriteB) = RuntimeSpriteEditor.SliceSprite(sliceSprite, sliceDirection);
- 
-             var partA = Instantiate(partUnitPrefab, unitPosition, unitRotation);
-             partA.transform.localScale = unitLocalScale;
-             partA.GetComponent<SpriteRenderer>().sprite = spriteA;
+            var partA = Instantiate(partUnitPrefab, unitPosition, unitRotation);
+            partA.transform.localScale = unitLocalScale;
+            partA.GetComponent<SpriteRenderer>().sprite = spriteA;
+            
+            var partB = Instantiate(partUnitPrefab, unitPosition, unitRotation);
+            partB.transform.localScale = unitLocalScale;
+            partB.GetComponent<SpriteRenderer>().sprite = spriteB;
              
-             var partB = Instantiate(partUnitPrefab, unitPosition, unitRotation);
-             partB.transform.localScale = unitLocalScale;
-             partB.GetComponent<SpriteRenderer>().sprite = spriteB;
-             
-             SplatterController.Instance.InstanceSplatter(unitPosition, Color.white);
-             
-             Destroy(unit);
-             
-             // TODO: velocity for partA and partB by direction           
-             
+            SplatterController.Instance.InstanceSplatter(unitPosition, Color.white);
+                         
+            Destroy(unit);
+                         
+            // TODO: velocity for partA and partB by sliceSecant
         }
     }
 }
--- Assets/Scripts/Utils/RuntimeSpriteEditor.cs
+++ Assets/Scripts/Utils/RuntimeSpriteEditor.cs
@@ -4,16 +4,17 @@ namespace Utils
 {
     public static class RuntimeSpriteEditor
     {
-        public static (Sprite, Sprite) SliceSprite(Sprite sprite, Vector2 direction)
+        public static (Sprite, Sprite) SpriteSlice(Sprite sprite, Vector2 secant)
         {
             var spriteA = Sprite.Create(sprite.texture, sprite.rect, sprite.pivot, sprite.pixelsPerUnit);
             var spriteB = Sprite.Create(sprite.texture, sprite.rect, sprite.pivot, sprite.pixelsPerUnit);
 
-            var vertices = new Vector2[3];
-            
-            // TODO: vertices by direction
+            var vertices = new Vector2[]
+            {
+                
+            };
 
-            var triangles = new ushort[] {0, 1, 2};
+            var triangles = new ushort[] { 0, 1, 2, 1, 3, 2 };
 
             spriteA.OverrideGeometry(vertices, triangles);
             
@@ -21,8 +22,17 @@ namespace Utils
             
             spriteB.OverrideGeometry(vertices, triangles);
            
-            // TODO: right return value
             return (sprite, sprite);
+        }
+
+        public static Vector2 SpritePivotUV(Sprite sprite)
+        {
+            return (new Vector2(sprite.rect.x, sprite.rect.y) + sprite.pivot) / sprite.rect.max;
+        }
+
+        public static Vector2 WorldPointToSpriteUV(Sprite sprite, Vector3 worldPoint, Transform transform)
+        {
+            return SpritePivotUV(sprite) + (Vector2) transform.InverseTransformPoint(worldPoint) * sprite.pixelsPerUnit;
         }
     }
 }
