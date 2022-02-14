a5a05774257a47b235f024350f84c836aaa69888
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-15 02:38:28 +0300

    added/init ScoreController and Score Component

---
+++ Assets/Scripts/ScriptableObjects/ScoreSettings.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "ScoreSettings", menuName = "Score Settings", order = 0)]
+    public class ScoreSettings : ScriptableObject
+    {
+        
+    }
+}
