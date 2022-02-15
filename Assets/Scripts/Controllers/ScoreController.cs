4caaf593f996de06c62d9470a7da6015f8b434be
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-15 15:35:01 +0300

    added set score logic and set score logic for ui

--- Assets/Scripts/Controllers/ScoreController.cs
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -1,10 +1,58 @@
-using ScriptableObjects;
+using Components;
 using UnityEngine;
 
 namespace Controllers
 {
     public class ScoreController : MonoBehaviour
     {
-        [SerializeField] private ScoreSettings settings;
+        public static ScoreController Instance;
+
+        public int CurrentScore { get; private set; }
+        public int BestScore { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void LoadScore()
+        {
+            CurrentScore = 0;
+            BestScore = 0;
+            
+            if (PlayerPrefs.HasKey("bestScore"))
+            {
+                BestScore = PlayerPrefs.GetInt("bestScore");
+            }
+            
+            UIController.Instance.SetScore(CurrentScore, BestScore);  
+        }
+
+        public void SetScore(Score score)
+        {
+            CurrentScore += score.Count;
+            
+            if (CurrentScore > BestScore)
+            {
+                BestScore = CurrentScore;
+                PlayerPrefs.SetInt("bestScore", BestScore);
+                PlayerPrefs.Save();
+            }
+            
+            UIController.Instance.SetScore(CurrentScore, BestScore);
+        }
+
+        public void ResetScore()
+        {
+            PlayerPrefs.DeleteKey("bestScore");
+        }
     }
 }
--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,14 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Controllers
 {
     public class UIController : MonoBehaviour
     {
         public static UIController Instance;
+        
+        [SerializeField] private GameObject scoreContainer;
+        [SerializeField] private List<Text> currentScoreCounts;
+        [SerializeField] private List<Text> bestScoreCounts;
+        [SerializeField] private GameObject heartsContainer;
+        [SerializeField] private GameObject gameOver;
 
         private void Awake()
         {
             Instance = this;
+        }
+
+        public void SetScore(int currentScore, int bestScore)
+        {
+            currentScoreCounts.ForEach(count => count.text = currentScore.ToString());
+            bestScoreCounts.ForEach(count => count.text = bestScore.ToString());
+        }
+
+        public void GameOver()
+        {
+            scoreContainer.SetActive(false);
+            heartsContainer.SetActive(false);
+            gameOver.SetActive(true);
         }
     }
 }
