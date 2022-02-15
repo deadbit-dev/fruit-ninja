603501c56ca42b58b24ca0d441f76b1226a84c4e
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-02-15 12:01:28 +0300

    added singleton classes

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -5,6 +5,21 @@ namespace Controllers
 {
     public class GameController : MonoBehaviour
     {
+        public static GameController Instance;
+        
         [SerializeField] private GameSettings settings;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Controllers
 {
     public class SceneController : MonoBehaviour
     {
-        
+        public static SceneController Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void SwitchScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
